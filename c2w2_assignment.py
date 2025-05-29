2707e1f9047f41d196a896017db7499b2e6fb363
Aayushi Tomar <aayushitomar1998@gmail.com>
2025-05-29 22:22:28 +0200

    Perform get() request to new releases endpoint

--- c2w2_assignment.py
+++ c2w2_assignment.py
@@ -42,6 +42,8 @@ def get_token(client_id: str, client_secret:str, url: str) -> Dict[Any, Any]:
         response.raise_for_status()
         response_json = json.loads(response.content)
 
+        return response_json
+
     except Exception as err:
         print(f"Error: {err}")
         return {}
@@ -50,8 +52,55 @@ def get_token(client_id: str, client_secret:str, url: str) -> Dict[Any, Any]:
 URL_TOKEN = "https://accounts.spotify.com/api/token"
 token = get_token(CLIENT_ID, CLIENT_SECRET, URL_TOKEN)
 
-print(token)
+#print(token)
+
+# Authorization header function 
+def get_auth_header(access_token: str) -> Dict[Any, Any]:
+    return {"Authorization": f"Bearer {access_token}"}
 
 
+# Some examples
+# 1. Get New Releases
+def get_new_releases(access_token: str, url: str, offset: int=0, limit: int=20, next: str="") -> Dict[Any, Any]:
+    """Allows to perform a GET request to get new releases from Spotify
+    
+    Args:
+        access_token(str): access token for authorization
+        url(str): Base URL for new releases 
+        offset(int, optional): Starting position(page offset for pagination). Defaults to 0
+        limit(int, optional): Number of elements per page. Defaults to 20
+        next(str, optional): Next URL to perform ext request.Defaults to ""
+        
+    Returns:
+        Dict[Any, Any]: Request response 
+        
+    """
+
+    if next == "":
+        request_url = f"{url}?offset={offset}&limit={limit}"
+    else:
+        request_url = f"{next}"
+
+    headers = get_auth_header(access_token)
+
+    try:
+        response = requests.get(url=request_url, headers=headers)
+        response.raise_for_status()
+        response_json = response.json()
+
+        return response_json
+    
+    except Exception as err:
+        print(f"Error: {err}")
+        return {}
+    
+URL_GET_NEW_RELEASES = "https://api.spotify.com/v1/browse/new-releases"
+new_releases = get_new_releases(access_token=token.get('access_token'), url=URL_GET_NEW_RELEASES)
+#print(new_releases) 
+
+#print(new_releases['albums'].keys())
+
+total = new_releases.get('albums').get('total')
+print(total)
 
 
